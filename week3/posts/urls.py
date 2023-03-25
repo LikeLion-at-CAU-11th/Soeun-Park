cc26fdd82f0fb7121b4a06c1dcbdaef8474a2ad3
soeun <separk314@naver.com>
2023-03-25 15:50:19 +0900

    Style: urls.py path 수정

--- week3/posts/urls.py
+++ week3/posts/urls.py
@@ -3,5 +3,5 @@ from posts.views import *
 
 urlpatterns = [
     path('', hello_world, name='hello_world'),
-    path('introduction', challenge, name='assignment_challenge'),
+    path('introduction/', challenge, name='assignment_challenge'),
 ]
