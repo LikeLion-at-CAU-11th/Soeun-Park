406d51de9cd416143c35be2ad2d0412018a2d781
soeun <separk314@naver.com>
2023-03-31 13:52:59 +0900

    Comment: models.py 주석 추가

--- assignment/posts/models.py
+++ assignment/posts/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
-
+# BaseModel 클래스: 공통으로 사용할 속성, 기능을 정의한 class
 class BaseModel(models.Model):
-    created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)
+    created_at = models.DateTimeField(verbose_name="작성일시", auto_now_add=True)   # auto_now_add: 최초 저장할 때의 날짜 적용(갱신 불가능)
+    updated_at = models.DateTimeField(verbose_name="수정일시", auto_now=True)   # auto_now: 모델이 저장될 때마다 저장 날짜 적용(갱신 가능)
     # verbose_name: admin에서 볼 때의 이름
 
-    # 기본으로 실행
     class Meta:
-        abstract = True
+        abstract = True # BaseModel을 상속 가능하도록
 
-class Post(BaseModel):
+class Post(BaseModel):  # BaseMode 클래스를 상속
 
     CHOICES = (
         ('DIARY', '일기'),
