6b9ce3ca2e44ba8cc6fb25241aaa0da3a5bb77a7
separk314 <separk314@naver.com>
2023-04-12 20:23:48 +0900

    commit

--- assignment/config/settings.py
+++ assignment/config/settings.py
@@ -82,8 +82,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 # 허용된 Cors 출처(클라이언트)
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
-    "http://127.0.0.1:3000"
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = "config.urls"
